2b9a6f7ba2d9e2c645c7326ef4403cdd62c3c5cf
nikolan92 <92nikolan@gmail.com>
2017-04-13 01:03:37 +0200

    Small changes.

--- PhotoEditor/ImageOperations/PixelateImage.cs
+++ PhotoEditor/ImageOperations/PixelateImage.cs
@@ -9,7 +9,12 @@ namespace PhotoEditor.ImageOperations
 {
     public class PixelateImage
     {
-
+        /// <summary>
+        /// Pixelate image, using source image to get image data, perform gamma on it and copy modified data into destinationImage. 
+        /// </summary>
+        /// <param name="sourceImage">Source image. This image will not be changed.</param>
+        /// <param name="destinationImage">Destination image. This image will be modified with gamma filter.</param>
+        /// <param name="pixelSize">Size of new pixel.</param>
         public static unsafe void PixelateImageUnsafeWithCopy(WriteableBitmap sourceImage, WriteableBitmap destinationImage, int pixelSize)
         {
             //------------------------------- DestinationImageParametars -----------------------------------
@@ -24,18 +29,14 @@ namespace PhotoEditor.ImageOperations
             int widthInPixels = sourceImage.PixelWidth;
             int stride = sourceImage.BackBufferStride;
 
-            //int stepX, stepY;
-            //stepX = widthInPixels/pixelSize;
-            //stepY = heightInPixels/pixelSize;
             int pixelSizeInBytesPerPixel;
+            int pixelSizeY = pixelSize;
             int aB, aG, aR, divider,tmpDivider;
             tmpDivider = divider = pixelSize*pixelSize;
-            aB = aG = aR = 0;
 
             int counterX, counterY;
             counterY = counterX = 0;
 
-            int pixelSizeY = pixelSize;
             for(int y = 0; y < heightInPixels; y = y + pixelSize)
             {
                 counterY++;
@@ -47,16 +48,17 @@ namespace PhotoEditor.ImageOperations
                 byte* tmpCurrentLineNew = currentLineNew;
 
 
-                //Reset for x iteration
+                //Reset parameters for x iterations.
                 counterX = 0;
                 pixelSizeInBytesPerPixel = bytesPerPixel * pixelSize;
-                //If pixelSizeY is changed divider is also changed (must be here)
+
                 tmpDivider = divider;// restore old divider
 
-                //If last line is reached then see how mouch is left and change Y boundary
+                //If last line is reached then see how much is left and change Y boundary.
                 if (pixelSize * counterY > heightInPixels)
                 {
                     pixelSizeY = heightInPixels - (pixelSize * (counterY - 1));
+                    //If pixelSizeY is changed divider is also changed.
                     tmpDivider = pixelSize * pixelSizeY;
                 }
 
@@ -71,13 +73,13 @@ namespace PhotoEditor.ImageOperations
                     currentLine += bytesPerPixel * x;
                     currentLineNew += bytesPerPixel * x;
 
-                    //If last pixel in line is reached then see how mouch is left and change Y boundary
+                    //If last pixel in line is reached then see how much is left and change X boundary.
                     if (pixelSizeInBytesPerPixel * counterX > widthInBytes)
                     {
                         pixelSizeInBytesPerPixel = widthInBytes - (pixelSizeInBytesPerPixel * (counterX - 1));
                         tmpDivider = (pixelSizeInBytesPerPixel / bytesPerPixel) * pixelSizeY;
                     }
-
+                    //Go throught old image and calculate average value for new pixel.
                     for (int i = 0; i < pixelSizeY; i++)
                     {
                         for (int j = 0; j < pixelSizeInBytesPerPixel; j = j + bytesPerPixel)
